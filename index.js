e83b04bd95b11b83e2a16663e70645c1cfb673b4
Aditya Saxena <aaddii091@gmail.com>
2023-02-09 01:04:28 +0530

    FNSUT

---
+++ index.js
@@ -0,0 +1,12 @@
+"strict";
+// -------------------------------sidenavbar
+const ham = document.getElementById("burger");
+function pop() {
+  ham.style.right = "0px";
+}
+function push() {
+  ham.style.right = "-200px";
+}
+function ok() {
+  ham.style.right = "-200";
+}
